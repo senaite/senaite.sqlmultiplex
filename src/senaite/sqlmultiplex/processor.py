06a0853835b52ef21d29e058298ee103f52a2280
Jordi Puiggené <jp@naralabs.com>
2020-11-13 15:02:26 +0100

    Cleanup

--- src/senaite/sqlmultiplex/processor.py
+++ src/senaite/sqlmultiplex/processor.py
@@ -8,6 +8,8 @@ from zope.interface import implementer
 
 from senaite.sqlmultiplex import check_installed
 
+
+# TODO Get this dynamically instead
 TYPES_TO_MULTIPLEX = (
     ("AnalysisRequest", [
         "ClientSampleID",
@@ -60,11 +62,15 @@ class CatalogMultiplexProcessor(object):
         params = ["host", "port", "database", "user", "password"]
         for param in params:
             reg_id = "senaite.sqlmultiplex.{}".format(param)
-            config.update(api.get_registry_record(reg_id))
+            config.update({param: api.get_registry_record(reg_id)})
         return config
 
     def get_default_attributes(self, obj):
+        """Returns the default attributes from the object to keep mapped
+        against the SQL database
+        """
         portal_type = api.get_portal_type(obj)
+        # TODO Get this from somewhere instead of a const
         return dict(TYPES_TO_MULTIPLEX).get(portal_type, [])
 
     def supports_multiplex(self, obj):
@@ -77,25 +83,24 @@ class CatalogMultiplexProcessor(object):
         if not self.supports_multiplex(obj):
             return
 
-        if attributes is None:
+        if not attributes:
             # Get the default columns for this obj
             attributes = self.get_default_attributes(obj)
 
         if not attributes:
+            logger.debug("SKIP. No attributes set for {}".format(repr(obj)))
             return
 
         logger.info("Multiplexer::Indexing {}".format(repr(obj)))
 
         # Insert the object to the SQL db
-        add, data = self.get_insert_sql(obj, attributes)
-        self.execute_sql(add, data)
+        operation, params = self.get_insert_operation(obj, attributes)
+        self.execute(operation, params)
 
     @check_installed
     def reindex(self, obj, attributes=None, update_metadata=1):
-        if not self.supports_multiplex(obj):
-            return
-
-        # TODO Update the object from the SQL db
+        # TODO Update the object from the SQL db?
+        self.index(obj, attributes=attributes)
         pass
 
     @check_installed
@@ -108,14 +113,18 @@ class CatalogMultiplexProcessor(object):
         # Do something here
         logger.info("Multiplexer::Unindexing {}".format(repr(obj)))
 
-    def execute_sql(self, statement, data):
+    def execute(self, operation, params=None):
+        """Executes the given database operation (query or command). The
+        parameters found in the tuple params are bound to the variables in the
+        operation. Specify variables using %s or %(name)s parameter style
+        :param operation: SQL query or command
+        :param data: variables for the operation
+        """
+        cnx = self.mysql_connection
+        cursor = cnx.cursor()
         try:
-            cnx = self.mysql_connection
-            cursor = cnx.cursor()
-            cursor.execute(statement, data)
+            cursor.execute(operation, params=params)
             cnx.commit()
-            cursor.close()
-            cnx.close()
             return True
         except mysql.connector.Error as err:
             host = self.get_connection_configuration().get("host")
@@ -123,18 +132,35 @@ class CatalogMultiplexProcessor(object):
                                                          err.sqlstate, err.msg)
             # 1146. Table ? does not exist
             logger.error(msg)
+        finally:
+            try:
+                logger.info(cursor.statement)
+            except:
+                pass
+            cursor.close()
+            cnx.close()
         return False
 
-    def get_insert_sql(self, obj, columns):
+    def get_insert_operation(self, obj, attributes):
+        """Returns a tuple of two values: SQL insert operation, and operation
+        parameters values. The name of the SQL table is the portal type.
+        Attributes represent both obj functions/attributes and SQL columns.
+        :param obj: the object to insert
+        :param attributes: attributes/columns from the object to be inserted
+        """
+        # TODO Use supermodel instead (this is wonky because depends on request)
         record = japi.get_info(obj, complete=True)
         portal_type = api.get_portal_type(obj)
 
         # Build the base insert thingy
         insert = ("INSERT INTO {} ({}) VALUES ({})".format(
-            portal_type, ", ".join(columns), ", ".join(["%s"]*len(columns))))
+            portal_type,
+            ", ".join(attributes),
+            ", ".join(["%s"]*len(attributes)))
+        )
 
         # Get the values for the columns
-        data = map(lambda column: record.get(column) or "", columns)
+        data = map(lambda column: record.get(column) or "", attributes)
         return insert, data
 
     def begin(self):
